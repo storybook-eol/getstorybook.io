178ff2f1611f37a7829cd105efe5bce7877ef032
thinkholic <mailtokasun@gmail.com>
2016-08-10 16:43:19 +0530

    add text-formatted logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,12 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './styles';
-import logo from './images/logo.png';
 
 class Logo extends Component {
   render() {
     return (
       <div style={ styles.wrapper }>
-        <img style={ styles.logo } src={logo} />
+        <div style={ styles.border }>
+          <span style={ styles.span.a }>S</span>
+          <span style={ styles.span.b }>T</span>
+          <span style={ styles.span.c }>O</span>
+          <span style={ styles.span.d }>R</span>
+          <span style={ styles.span.e }>Y</span>
+          <span style={ styles.span.f }>B</span>
+          <span style={ styles.span.g }>O</span>
+          <span style={ styles.span.h }>O</span>
+          <span style={ styles.span.i }>K</span>
+        </div>
       </div>
     );
   }
