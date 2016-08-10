1e268d7a732f2fc75a73aaf0ee964a5074be04f2
thinkholic <mailtokasun@gmail.com>
2016-08-10 12:50:29 +0530

    add Footer Comp.

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,26 @@
+export default {
+  wrapper: {
+    width: '800px',
+    margin: 'auto',
+    border: '1px solid #ccc',
+    boxShadow: 'rgb(200, 200, 200) 0px 2px 4px',
+  },
+
+  ul: {
+    margin: 0,
+    padding: 0,
+    listStyle: 'none',
+  },
+
+  li: {
+    all: {
+      padding: '15px 20px',
+      cursor: 'pointer',
+      borderTop: '1px solid #ccc',
+    },
+    first: {
+      padding: '15px 20px',
+      cursor: 'pointer',
+    },
+  },
+};
