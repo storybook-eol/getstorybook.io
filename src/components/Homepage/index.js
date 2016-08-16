6b66f12edd6fadb2bce940bbacc0a9b50d042bb1
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-16 23:13:13 +0530

    git commit -am

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import Header from './Header';
 import Heading from './Heading';
 import Demo from './Demo';
@@ -11,6 +12,10 @@ import Footer from './Footer';
 
 const Homepage = () => (
   <div>
+    <Helmet
+      htmlAttributes={{"lang": "en", "amp": undefined}}
+      title="Storybook - UI Dev Environment you'll love to use."
+    />
     <Header />
     <Heading />
     <Demo />
