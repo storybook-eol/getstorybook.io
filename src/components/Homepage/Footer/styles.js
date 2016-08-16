bb64abee852d572afc083a0ac184c67856e67c4d
thinkholic <mailtokasun@gmail.com>
2016-08-16 14:29:45 +0530

    update footer styles

--- src/components/Homepage/Footer/styles.js
+++ src/components/Homepage/Footer/styles.js
@@ -3,7 +3,7 @@ import theme from '../../theme';
 export default {
   container: {
     textAlign: 'center',
-    marginTop: 40,
+    marginTop: '6%',
     borderTop: '1px solid #EEE',
     padding: 40,
   },
