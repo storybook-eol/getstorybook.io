b8ffc28b8b38865e6e04073317e9932e7ec81667
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-18 10:11:54 +0530

    Add styling for Docs.

--- src/components/Docs/DocsHeader/index.js
+++ src/components/Docs/DocsHeader/index.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import Container from '../Container';
+import styles from './styles';
 
 const DocsHeader = () => (
   <Container>
-    <div>
-      <h1>Documentation for React Storybook</h1>
-      <p>
+    <div style={styles.container}>
+      <h1 style={styles.h1}>Documentation for React Storybook</h1>
+      <div style={styles.links}>
+        Docs for:&nbsp;
         <a
-          href="https://github.com/kadirahq/react-native-storybook" 
+          style={styles.link}
+          href="https://github.com/kadirahq/react-native-storybook"
           target="_blank"
         >
           React Native
-        </a> | &nbsp;
-        <a
-          href=""
-          target="_blank"
-        >
-          Vue
-        </a> | &nbsp;
-        <a
-          href=""
-          target="_blank"
-        >
-          Angular2
         </a>
-      </p>
+      </div>
     </div>
   </Container>
 );
---
+++ src/components/Docs/DocsHeader/styles.js
@@ -0,0 +1,25 @@
+import theme from '../../theme';
+
+export default {
+  container: {
+    ...theme.base,
+    padding: "0 0 45px 0"
+  },
+
+  h1: {
+    fontSize: 35,
+    margin: "0 0 5px 0",
+    padding: 0,
+  },
+
+  links: {
+    ...theme.text,
+    fontSize: 20,
+  },
+
+  link: {
+    ...theme.text,
+    textDecoration: 'none',
+
+  }
+};
