e2950c8f53a628e4e07869105950ad7db68c98dc
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-18 16:37:20 +0530

    Add better markdown support.

--- src/components/Docs/Content/index.js
+++ src/components/Docs/Content/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import Highlight from 'react-highlight';
 import styles from './styles';
+import 'highlight.js/styles/atom-one-light.css';
+
+import marked from 'marked';
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
 
 const Content = ({title, content}) => (
   <div style={ styles.container }>
     <h2 style={ styles.h2 }>{ title }</h2>
-    <ReactMarkdown source={content} />
+    <Highlight innerHTML={true}>
+      {marked(content)}
+    </Highlight>
   </div>
 );
 
