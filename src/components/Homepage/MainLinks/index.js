07f6ca04685afd1b5ae09330742b553b4f5f1b2e
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-18 16:21:01 +0530

    Update MainLinks to use RR.

--- src/components/Homepage/MainLinks/index.js
+++ src/components/Homepage/MainLinks/index.js
@@ -2,18 +2,43 @@ import React from 'react';
 import styles from './styles';
 import { Flex, Box } from 'reflexbox';
 import Container from '../Container';
+import { Link } from 'react-router';
 
-export const Link = ({ icon, caption, href }) => (
-  <div style={styles.link}>
-    <a style={styles.a} href={href}>
-      <img style={styles.icon} src={icon} role="presentation"/>
-      <br/>
-      { caption }
-    </a>
-  </div>
-);
 
-Link.propTypes = {
+class ATag extends React.Component {
+  renderLocalLink() {
+    const { icon, caption, href } = this.props;
+    return (
+      <Link style={styles.a} to={href}>
+        <img style={styles.icon} src={icon} role="presentation"/>
+        <br/>
+        { caption }
+      </Link>
+    );
+  }
+
+  renderOutsideLink() {
+    const { icon, caption, href } = this.props;
+    return (
+      <a style={styles.a} href={href}>
+        <img style={styles.icon} src={icon} role="presentation"/>
+        <br/>
+        { caption }
+      </a>
+    );
+  }
+
+  render() {
+    const { href } = this.props;
+    return (
+      <div style={styles.link}>
+        {/^http/.test(href)? this.renderOutsideLink() : this.renderLocalLink()}
+      </div>
+    );
+  }
+}
+
+ATag.propTypes = {
   link: React.PropTypes.string,
   caption: React.PropTypes.string,
   href: React.PropTypes.string,
@@ -24,21 +49,21 @@ const MainLinks = () => (
     <div style={styles.container}>
       <Flex>
         <Box>
-          <Link
+          <ATag
             icon={require('../../../design/homepage/slack-icon.png')}
             caption="Join Us on Slack"
             href="https://storybooks-slackin.herokuapp.com/"
           />
         </Box>
         <Box>
-          <Link
+          <ATag
             icon={require('../../../design/homepage/docs-icon.png')}
             caption="Read Docs"
             href="/docs"
           />
         </Box>
         <Box>
-          <Link
+          <ATag
             icon={require('../../../design/homepage/mail-icon.png')}
             caption="Get Newsletter"
             href="https://tinyletter.com/storybooks"
