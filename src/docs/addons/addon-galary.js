87c998d5ded3c6bb7fc7ed63e97be754305bd5ac
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-30 07:57:29 +0530

    Update Addon Galary docs.

--- src/docs/addons/addon-galary.js
+++ src/docs/addons/addon-galary.js
@@ -6,21 +6,21 @@ export default {
   content: stripIndent`
     This is a list of available addons for Storybook.
 
-    ## Built In Addons.
+    ## Built-In Addons.
 
     These addons ship with Storybook by default. You can use them right away.
 
     ### [Actions](https://github.com/kadirahq/storybook-addon-actions)
 
-    With actions, you can inspect events of your components. This is pretty neat when you are manually testing your components.
+    With actions, you can inspect events related to your components. This is pretty neat when you are manually testing your components.
 
-    Also, you can think this as a way to document events in your components.
+    Also, you can think of this as a way to document events in your components.
 
     ### [Links](https://github.com/kadirahq/storybook-addon-links)
 
     With links you can link stories together. With that, you can build demos and prototypes directly from your UI components. (Like you can do with [InVision](https://www.invisionapp.com/) and [Framer.js](https://framerjs.com/))
 
-    ## Thrid Party Addons
+    ## Third Party Addons
 
     You need to install these addons directly from NPM in order to use them.
 
@@ -35,6 +35,6 @@ export default {
 
     ### [Info](https://github.com/kadirahq/react-storybook-addon-info)
 
-    If you are using Storybook as a style guide, then this addon will help you to build a nice looking Style Guide with docs, automatic sample source code and with a PropTypes explorer.
+    If you are using Storybook as a style guide, then this addon will help you to build a nice-looking style guide with docs, automatic sample source code with a PropType explorer.
   `
 };
