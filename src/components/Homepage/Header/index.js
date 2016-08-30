706c75d239b462deeb38903cff0778589da2595f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-31 05:18:49 +0530

    Update images.

--- src/components/Homepage/Header/index.js
+++ src/components/Homepage/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './style.css';
 
+import storybookLogo from '../../../design/homepage/storybook-logo.png';
+
 const sections = [
   { id: 'home', caption: 'Home', href: '/' },
   { id: 'docs', caption: 'Docs', href: '/docs' },
@@ -25,11 +27,19 @@ class Header extends React.Component {
   }
 
   render() {
+    const { currentSection } = this.props;
+    let titleClassname = 'pull-left';
+    if (currentSection === 'home') {
+      titleClassname += ' hide';
+    }
+
     return (
       <div id="header" className="row">
         <div className="col-xs-12">
-          <div id="header-title" className="pull-left">
-            <a href="/">STORYBOOK</a>
+          <div id="header-title" className={titleClassname}>
+            <a href="/">
+              <img className="sb-title" src={storybookLogo} alt="Storybook Logo"/>
+            </a>
           </div>
           <div id="header-links" className="pull-right">
             { this.renderSections() }
