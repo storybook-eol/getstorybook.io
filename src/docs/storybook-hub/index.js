f22fbd4bb3ed62253405e5436ce23daefc8f777f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-10-19 16:43:39 +0530

    Add management features section.

--- src/docs/storybook-hub/index.js
+++ src/docs/storybook-hub/index.js
@@ -10,4 +10,15 @@ export default [
       require('./basics/security').default,
     ]
   },
+
+  {
+    id: 'management-features',
+    heading: 'Management Features',
+    items: [
+      require('./management-features/workspaces').default,
+      require('./management-features/sharing-storybooks').default,
+      require('./management-features/env-variables').default,
+      require('./management-features/private-npm-packages').default,
+    ]
+  },
 ];
