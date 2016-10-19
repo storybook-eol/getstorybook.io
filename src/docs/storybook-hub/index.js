17859ceafa6f64b75e494cdfb0b0e95c075c125c
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-10-19 16:27:51 +0530

    Add all basics docs.

--- src/docs/storybook-hub/index.js
+++ src/docs/storybook-hub/index.js
@@ -4,6 +4,10 @@ export default [
     heading: 'Basics',
     items: [
       require('./basics/getting-started').default,
+      require('./basics/github-pr-integration').default,
+      require('./basics/comments').default,
+      require('./basics/private-or-public-apps').default,
+      require('./basics/security').default,
     ]
   },
 ];
