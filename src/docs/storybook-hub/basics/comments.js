4f0fec9a52f7297320816ff6bfdaab1c3b7d1517
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-10-20 16:19:38 +0530

    Add the comment screenshot.

--- src/docs/storybook-hub/basics/comments.js
+++ src/docs/storybook-hub/basics/comments.js
@@ -1,11 +1,15 @@
 import {stripIndent} from 'common-tags';
 
+import commentsInsideStorybookImage from './static/comments-inside-storybook.png';
+
 export default {
   id: "comments",
   title: "Comments",
   content: stripIndent`
     You can comment inside Storybooks and discuss with your team. Comments are namespaced based on the git branch.
     So, any storybook associated with a particular branch has the same set of comments.
+
+    ![Comment Inside Storybook](${commentsInsideStorybookImage})
 
     Adding comments support is easy. Add following NPM packages into your app:
 
